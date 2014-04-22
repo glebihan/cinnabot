b181e6247067f9a81f4de873a3c2cacf55ca95da
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2014-04-22 11:35:07 +0200

    Display number of builds started

--- Cinnabot/plugins/EasyLRH.py
+++ Cinnabot/plugins/EasyLRH.py
@@ -17,12 +17,12 @@ class EasyLRHPlugin(BasePlugin):
                 url += "&target=" + distro_code
             c = httplib2.Http()
             c.add_credentials(self._get_config("easylrh_username"), self._get_config("easylrh_password"))
-            c.request(url)
+            resp, content = c.request(url)
             if target.startswith("#"):
                 resp_target = target
             else:
                 resp_target = source.split("!")[0]
-            return self.privmsg_response(resp_target, "Build request sent")
+            return self.privmsg_response(resp_target, content)
     
     def process_privmsg(self, source, target, msg):
         return self.process_highlight(source, target, msg)
