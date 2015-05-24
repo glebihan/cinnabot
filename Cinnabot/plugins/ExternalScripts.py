0a77adf552b1bd840b7d7e39d7c34ac7f06dff60
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2015-05-24 14:34:47 +0200

    Report failures on ExternalScriptsPlugin

--- Cinnabot/plugins/ExternalScripts.py
+++ Cinnabot/plugins/ExternalScripts.py
@@ -2,11 +2,19 @@
 # -*- coding=utf-8 -*-
 
 from Cinnabot.BasePlugin import BasePlugin
-import os
+import subprocess
 
 class ExternalScriptsPlugin(BasePlugin):
     def __init__(self, bot, plugin_name):
         BasePlugin.__init__(self, bot, plugin_name)
+    
+    def _run_command(self, from_username, resp_target, command_name, exec_command, exec_params):
+        status = subprocess.call([exec_command] + exec_params)
+        if status != 0:
+            resp_msg = "%s failed" % command_name
+            if resp_target.startswith("#"):
+                resp_msg = from_username + ", " + resp_msg
+            return self.privmsg_response(resp_target, resp_msg)
     
     def process_highlight(self, from_username, source, target, msg):
         res = []
@@ -16,14 +24,12 @@ class ExternalScriptsPlugin(BasePlugin):
             command = self._get_config("command%d" % i)
             command_name, start_feedback, exec_command = command.split(":", 3)
             if msg.split(" ")[0] == command_name:
-                if os.fork() == 0:
-                    os.execvp(exec_command, (exec_command,) + tuple(msg.split(" ")[1:]))
+                if target.startswith("#"):
+                    resp_target = target
                 else:
-                    if target.startswith("#"):
-                        resp_target = target
-                    else:
-                        resp_target = source.split("!")[0]
-                    res.append(self.privmsg_response(resp_target, start_feedback))
+                    resp_target = source.split("!")[0]
+                self._start_task(self._run_command, from_username, resp_target, command_name, exec_command, msg.split(" ")[1:])
+                res.append(self.privmsg_response(resp_target, start_feedback))
             i += 1
         return res
     
