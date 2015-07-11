b3a96260f8aaac2286e878b9dc1fecfafa0efc82
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2015-07-11 18:44:01 +0200

    CommunityRegistration: Ignore quotes in messages

--- Cinnabot/plugins/CommunityRegistration.py
+++ Cinnabot/plugins/CommunityRegistration.py
@@ -68,7 +68,7 @@ class CommunityRegistrationPlugin(BasePlugin):
             return self.privmsg_response(source.split("!")[0], "OK, I won't send you registration codes anymore")
         
     def process_channel_message(self, source, target, msg):
-        words = msg.split()
+        words = msg.replace("'", "").replace('"', "").split()
 
         current_word = ""
         words_lower = []
@@ -82,7 +82,7 @@ class CommunityRegistrationPlugin(BasePlugin):
         if current_word != "":
             words_lower.append(current_word)
         
-        if ("d'enregistrement" in words_lower and "code" in words_lower) or ("registro" in words_lower and (u'c\ufffddigo' in words_lower or 'codigo' in words_lower)) or ("registration" in words_lower and "code" in words_lower) or ("community" in words_lower and "code" in words_lower) or ("registration" in words_lower and "community" in words_lower) or ("reg code" in msg.lower()) or ("reg. code" in msg.lower()):
+        if (("denregistrement" in words_lower or "enregistrement" in words_lower) and "code" in words_lower) or ("registro" in words_lower and (u'c\ufffddigo' in words_lower or 'codigo' in words_lower)) or ("registration" in words_lower and "code" in words_lower) or ("community" in words_lower and "code" in words_lower) or ("registration" in words_lower and "community" in words_lower) or ("reg code" in msg.lower()) or ("reg. code" in msg.lower()):
             from_nickname = source.split("!")[0]
             if from_nickname in self._bot._nick_to_username_map and self._bot._nick_to_username_map[from_nickname] in self._ignore_users:
                 return
